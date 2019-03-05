2ce029c2a4401b333ab8b71e6eefb89b8420f211
daisuke <dsaito85a@gmail.com>
2019-03-05 19:40:55 +0900

    ROOM3no 表示を書き込む

--- src/_ajax.php
+++ src/_ajax.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/config.php';
 $contents = $_GET['contents'];
 $x = $_GET['x'];
 $y = $_GET['y'];
-
+$room = $_GET['room'];
 
 if (!isset($_GET['contents']) || !isset($_GET['x']) || !isset($_GET['y'])) {
     throw new \Exception('[create] title not set!');
@@ -15,14 +15,17 @@ try {
     //TODO:MAMP環境から本番環境に書き換え必要
     //    $pdo = new PDO('mysql:dbname=everyone-toilet;host=localhost;charset=utf8', 'root', 'root');
     $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
- 
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $pdo->prepare("INSERT INTO graffitis (
-  contents, x, y
-) VALUES (:contents, :x, :y)");
+  contents, x, y , room ,clean
+) VALUES (:contents, :x, :y , :ROOM, :clean)");
 
     $stmt->bindParam(':contents', $_GET['contents'], PDO::PARAM_STR);
     $stmt->bindValue(':x', $_GET['x'], PDO::PARAM_INT);
     $stmt->bindValue(':y', $_GET['y'], PDO::PARAM_INT);
+    $stmt->bindValue(':ROOM', $_GET['room'], PDO::PARAM_INT);
+    $stmt->bindValue(':clean', 0, PDO::PARAM_INT);
 
     $stmt->execute();
 
@@ -36,6 +39,6 @@ try {
 // 接続を閉じる
 $pdo = null;
 
-$str = "AJAX REQUEST SUCCESS" . " contens=" . $contents . " x=" . $x . " y=" . $y;
+$str = "AJAX REQUEST SUCCESS" . " contens=" . $contents . " x=" . $x . " y=" . $y ." room=" . $room;
 $result = nl2br($str);
 echo $result;
