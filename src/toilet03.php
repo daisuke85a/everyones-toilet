8f1fce642eda716d61e13bc318049b4af6ad092b
daisuke <dsaito85a@gmail.com>
2019-03-09 13:10:38 +0900

    前回掃除以前の落書きは表示しないよう修正

--- src/toilet03.php
+++ src/toilet03.php
@@ -9,24 +9,27 @@ if (SERVER_PATH !== $_SERVER['HTTP_REFERER']) {
 
     // すべての出力を終了
     exit;
-}
-else{
-  // echo "OK";
+} else {
+    // echo "OK";
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113487092-5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-113487092-5');
-</script>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113487092-5"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-113487092-5');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -39,16 +42,20 @@ else{
     <meta property="og:title" content="みんなのトイレ" />
     <meta property="og:description" content="ちょっと一人になりたいとき。泣きたいとき。ほ、っと一息つきたいとき。人はトイレにいくのです。" />
     <meta property="og:image" content="https://everyones-toilet.tokyo/images/ogp.png" />
-  </head>
+</head>
 
-  <body>
+<body>
     <main class="room">
-      <section class="room-wall">
-        <?php
+        <section class="room-wall">
+            <?php
 require_once __DIR__ . '/config.php';
 
 $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
-$stmt = $pdo->query("SELECT * FROM graffitis WHERE ROOM = 3");
+
+$stmt = $pdo->query('SELECT * from cleans WHERE kind = "last"');
+$lastCleanTime = $stmt->fetch(PDO::FETCH_ASSOC)["datetime"];
+$stmt = $pdo->prepare('SELECT * FROM graffitis WHERE ROOM = ? AND datetime > ?');
+$stmt->execute(array(3, $lastCleanTime));
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
@@ -61,21 +68,23 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 }
 
 ?>
-      </section>
-      <section class="room-floor">
-        <img class="toilet" src="images/toilet03.png" alt="3番目の個室のトイレ">
-        <div class="room-floor-btn-area">
-          <a class="sns-btn active-btn" href="http://twitter.com/intent/tweet?text=3番目の個室に入りました%0a&hashtags=みんなのトイレ&url=https://everyones-toilet.tokyo" onclick="window.open(this.href, 'tweetwindow', 'width=550, height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1'); return false;">
-            トイレ報告
-          </a>
-          <a href="index.html">トイレから出る</a>
-        </div>
-      </section>
+        </section>
+        <section class="room-floor">
+            <img class="toilet" src="images/toilet03.png" alt="3番目の個室のトイレ">
+            <div class="room-floor-btn-area">
+                <a class="sns-btn active-btn"
+                    href="http://twitter.com/intent/tweet?text=3番目の個室に入りました%0a&hashtags=みんなのトイレ&url=https://everyones-toilet.tokyo"
+                    onclick="window.open(this.href, 'tweetwindow', 'width=550, height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1'); return false;">
+                    トイレ報告
+                </a>
+                <a href="index.html">トイレから出る</a>
+            </div>
+        </section>
     </main>
     <footer>
-      &copy;みんなのトイレ
+        &copy;みんなのトイレ
     </footer>
     <script src="script.js"></script>
-  </body>
+</body>
 
 </html>
