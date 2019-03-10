a30048b6ef18e16de5e015469fe82a88fd350ce4
daisuke <dsaito85a@gmail.com>
2019-03-11 08:35:31 +0900

    掃除アニメーション中は個室を開けなくする

--- src/script.js
+++ src/script.js
@@ -11,23 +11,74 @@ function aTagClickChangeValid(valid) {
     console.log("aTagClickChangeValid true");
     for (var i = 0, len = list.length; i < len; i++) {
       list.item(i).removeEventListener("click", invalidEvent);
+
+      // list.item(i).style.backgroundImage = 'url(images/toilet-open.png)';
+      list.item(i).addEventListener("mouseover", function () {
+        event.preventDefault();
+        console.log("mouseover");
+        this.style.backgroundImage = 'url(images/toilet-open.png)';
+      })
+      list.item(i).addEventListener("mouseenter", function () {
+        event.preventDefault();
+        console.log("mouseenter");
+        this.style.backgroundImage = 'url(images/toilet-open.png)';
+      })
+
+      //mouseが領域から外れたらドアを閉じる
+      list.item(i).addEventListener("mouseout", function () {
+        event.preventDefault();
+        console.log("mouseout");
+        this.style.backgroundImage = 'url(images/toilet-close.png)';
+      })
+
     }
   } else {
     console.log("aTagClickChangeValid false");
     for (var i = 0, len = list.length; i < len; i++) {
       //aタグ要素のclickをすべて無効化する
       list.item(i).addEventListener("click", invalidEvent);
+
+      // list.item(i).style.backgroundImage = 'url(images/toilet-open.png)';
+      list.item(i).addEventListener("mouseover", function () {
+        event.preventDefault();
+        console.log("mouseover");
+        this.style.backgroundImage = 'url(images/toilet-close.png)';
+      })
+      list.item(i).addEventListener("mouseenter", function () {
+        event.preventDefault();
+        console.log("mouseenter");
+        this.style.backgroundImage = 'url(images/toilet-close.png)';
+      })
+      list.item(i).addEventListener("mouseout", function () {
+        event.preventDefault();
+        console.log("mouseout");
+        this.style.backgroundImage = 'url(images/toilet-close.png)';
+      })
+
     }
+
+    var toilet2 = document.getElementById('toilet2');
+    console.log("toilet2");
+    console.log(toilet2);
+    toilet2.classList.remove('hover');
   }
 }
 
 function confirmCleaningWithAjax() {
   var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       //通信が完了した時
       if (xhr.status === 200) {
+
         console.log("掃除アニメーション表示する？= " + xhr.responseText);
+        //jQuery使わない場合
+        var el = document.querySelector('.obachan01-active');
+        el.addEventListener('animationend', function () {
+          // animation終了時の処理
+          aTagClickChangeValid(true);
+        });
+
       } else {
         //通信が失敗した時
       }
@@ -41,11 +92,11 @@ function confirmCleaningWithAjax() {
   xhr.send(null);
 }
 
-$(function() {
+$(function () {
   console.log("hello");
   var num = 0;
 
-  $(".room-wall").on("click", function(e) {
+  $(".room-wall").on("click", function (e) {
     console.log("screen=" + e.screenX + "," + e.screenY);
     console.log("page=" + e.pageX + "," + e.pageY);
     console.log("client=" + e.clientX + "," + e.clientY);
@@ -71,22 +122,22 @@ $(function() {
     num++;
     $(".room-wall").append(
       '<form action="" id="new_todo_form_' +
-        num +
-        '"><input type="text" id="new_todo_' +
-        num +
-        '" style="position: absolute; top: ' +
-        (Number(offsetY) - 0) +
-        "px; left: " +
-        Number(offsetX - 8) +
-        'px;"></form>'
+      num +
+      '"><input type="text" id="new_todo_' +
+      num +
+      '" style="position: absolute; top: ' +
+      (Number(offsetY) - 0) +
+      "px; left: " +
+      Number(offsetX - 8) +
+      'px;"></form>'
     );
 
     $(".room-wall").append(
       '<div id="fukidashi"; style="position: absolute; top: ' +
-        (Number(offsetY) - 40) +
-        "px; left: " +
-        Number(offsetX - 8) +
-        'px;">落書きできるよ〜<span></span></form>'
+      (Number(offsetY) - 40) +
+      "px; left: " +
+      Number(offsetX - 8) +
+      'px;">落書きできるよ〜<span></span></form>'
     );
     console.log(e);
 
@@ -94,7 +145,7 @@ $(function() {
 
     document
       .getElementById("new_todo_form_" + num)
-      .addEventListener("submit", function(event) {
+      .addEventListener("submit", function (event) {
         event.preventDefault();
         console.log("addEventListener");
         var roomNum = Number(
@@ -109,15 +160,15 @@ $(function() {
         //ajax処理
 
         $.ajax({
-          url: "./_ajax.php",
-          type: "GET",
-          data: {
-            contents: $("#new_todo_" + num).val(),
-            x: offsetX,
-            y: offsetY,
-            room: roomNum
-          }
-        })
+            url: "./_ajax.php",
+            type: "GET",
+            data: {
+              contents: $("#new_todo_" + num).val(),
+              x: offsetX,
+              y: offsetY,
+              room: roomNum
+            }
+          })
           // Ajaxリクエストが成功した時発動
           .done(data => {
             console.log("ajax done");
@@ -148,53 +199,5 @@ $(function() {
         //画面のリフレッシュを防ぐためにreturn falseする
         return false;
       });
-
-    // //入力フォームでEnterが押されたら呼ばれる関数
-    // $("#new_todo_form_" + num).on("submit", function(e) {
-    //   event.preventDefault(); //submitが実行されると、画面が必ず更新されるというブラウザの仕様をキャンセルする
-
-    //   console.log(e);
-    //   console.log(event.target.baseURI);
-    //   console.log("submit #new_todo_form_" + num);
-    //   console.log($("#new_todo_form_" + num).val());
-    //   console.log($("#new_todo_" + num).val());
-
-    //   //ajax処理
-
-    //   $.ajax({
-    //     url: "./_ajax.php",
-    //     type: "GET",
-    //     data: {
-    //       contents: $("#new_todo_" + num).val(),
-    //       x: offsetX,
-    //       y: offsetY
-    //     }
-    //   })
-    //     // Ajaxリクエストが成功した時発動
-    //     .done(data => {
-    //       console.log("ajax done");
-    //       console.log(data);
-
-    //       //入力された落書きをp要素で追加。（勉強がてら、一部で生javasriptで書いている。設計上の意味はない。）
-    //       var p = document.createElement('p');
-    //       p.style= 'position: absolute; top: ' + offsetY + 'px; left: ' + offsetX + 'px;';
-    //       p.textContent = $("#new_todo_" + num).val();
-    //       document.getElementsByClassName("room-wall")[0].appendChild(p);
-
-    //     })
-    //     // Ajaxリクエストが失敗した時発動
-    //     .fail(data => {
-    //       console.log("ajax fail");
-    //     })
-    //     // Ajaxリクエストが成功・失敗どちらでも発動
-    //     .always(data => {
-    //       //落書き入力フォームを削除
-    //       document.getElementById("new_todo_form_" + num ).remove();
-    //       document.getElementById("fukidashi").remove();
-    //     });
-
-    //   //画面のリフレッシュを防ぐためにreturn falseする
-    //   return false;
-    // });
   });
 });
